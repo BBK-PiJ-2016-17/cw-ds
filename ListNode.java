d9e592864b7d7537b71e8ce0ab07378967c40a97
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-04 18:42:23 +0000

    Adding the node for the LinkedList

---
+++ ListNode.java
@@ -0,0 +1,43 @@
+/**
+ * Class ListNode for LinkedList exercise
+ *
+ * @author Ginestra Ferraro
+ */
+
+public class ListNode {
+	
+	private Object listItem;
+	private ListNode next;
+	private ListNode prev;
+	
+	// Constructor
+	public ListNode(Object listItem) {
+		this.listItem = listItem;
+		next = null;
+		prev = null;
+	}
+	
+	public void setObject(Object listItem) {
+		this.listItem = listItem;
+	}
+	
+	public void setNext(ListNode next) {
+		this.next = next;
+	}
+	
+	public void setPrev(ListNode prev) {
+		this.prev = prev;
+	}
+	
+	public Object getListItem() {
+		return listItem;
+	}
+	
+	public ListNode getNext() {
+		return next;
+	}
+	
+	public ListNode getPrev() {
+		return prev;
+	}
+}

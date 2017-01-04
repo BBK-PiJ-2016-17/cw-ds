444c2c73b3c0747a5864517c611d9b17892e0c09
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-04 16:43:21 +0000

    Added a test for ReturnObjectImpl

---
+++ ReturnObjectImplTest.java
@@ -0,0 +1,24 @@
+/**
+ * Testing ReturnObjectImpl
+ *
+ * @author Ginestra Ferraro
+ */
+
+public class ReturnObjectImplTest {
+	public static void main(String[] args) {
+
+		ReturnObject result = new ReturnObjectImpl("1st");
+		
+		if (result.hasError()) {
+			System.out.println("Error error!");
+		}
+		if (! result.getError().equals(ErrorMessage.NO_ERROR)) {
+			System.out.println("Error errror!");
+		}
+		if (! result.getReturnValue().equals("1st")) {
+			System.out.println("Error error!");
+		} else {
+			System.out.println("Tests passed. All good!");
+		}
+	}
+}

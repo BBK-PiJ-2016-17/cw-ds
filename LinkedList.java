03ced496bc2497be75d271fc170c5e42fb669f68
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-06 10:52:59 +0000

    Adding LinkedList exercise

---
+++ LinkedList.java
@@ -0,0 +1,138 @@
+/**
+ * A list is a collection of objects that are sorted and can be
+ * accessed by index. The first element in the list is at index 0.
+ *
+ * A list can store objects of any kind, and they can be of different
+ * types: Integers, Doubles, String, or even other lists. However,
+ * this list cannot store null objects.
+ * 
+ * There is no limit to the number of elements in the list (provided
+ * that there is free memory in the Java Virtual Machine).
+ * 
+ * Not all operations on a list will always be successful. For
+ * example, a programmer may try to remove an element from an empty
+ * list, or from a position where there is nothing. Since we hace not
+ * covered exceptions yet, we need another mechanism to report
+ * errors. In order to do that, methods of this list will return a
+ * {@see ReturnObject} that will contain either an object or an error
+ * value of the right kind (as defined in {@see ErrorMessage}). 
+ * 
+ * @author Ginestra Ferraro
+ */
+
+public class LinkedList implements List {
+	private ListNode firstItem;
+	private ListNode lastItem;
+	private int size;
+
+	// Constructor	
+	public LinkedList() {
+		this.firstItem = null;
+		this.lastItem = null;
+		this.size = 0;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public ReturnObject get(int index) {
+		if (isEmpty()) {
+			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		} else if (index < 0 || index >= size) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else {
+			ListNode current = firstItem;
+			return new ReturnObjectImpl(current);
+		}
+	}
+
+	@Override
+	public ReturnObject remove(int index) {
+		if (isEmpty()) {
+			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		} else if (index < 0 || index >= size()) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else {
+			ReturnObject objectRemove = get(index);
+			
+			ListNode nodeRemove = firstItem;
+
+			if (index == 0) {
+				nodeRemove.getNext().setNext(null);
+			} else if (index == size - 1) {
+				nodeRemove.getNext().setPrev(null);
+			} else {
+				nodeRemove.getNext().setNext(nodeRemove.getNext());
+				nodeRemove.getNext().setPrev(nodeRemove.getPrev());
+			}
+
+			size = size() - 1;
+			
+			return objectRemove;
+		}	
+	}
+
+	@Override
+	public ReturnObject add(int index, Object item) {
+		if (index < 0 || index >= size) {
+			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else if (item == null) {
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		} else if (index == 0) {
+			ListNode myList = firstItem;
+			myList.setNext(firstItem);
+			firstItem.setPrev(myList);
+			firstItem = myList;
+			size = size() + 1;
+			return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
+		}
+
+		ListNode current = firstItem;
+
+		for (int i = 0; i < (index - 1); i++) {
+			current = current.getNext();
+		}
+
+		ListNode myList = firstItem;
+		myList.setNext(myList.getNext());
+		myList.getNext().setPrev(myList);
+		myList.setNext(myList);
+		myList.setPrev(myList);
+		size = size() + 1;
+
+		return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
+	}
+
+	@Override
+	public ReturnObject add(Object item) {
+
+		if(item == null) {
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		} else {
+			ListNode newListNode = new ListNode(item);
+
+			if(isEmpty()) {
+				firstItem = newListNode;
+			} else {
+				newListNode.setPrev(newListNode.getPrev());
+				firstItem.setNext(newListNode);
+			}
+
+			size = size() + 1;
+			
+			return new ReturnObjectImpl(ErrorMessage.NO_ERROR);
+		}
+	}
+}
---
+++ LinkedListTest.java
@@ -0,0 +1,51 @@
+public class LinkedListTest {
+	public static void main(String[] args) {
+
+		List newLinkedList = new LinkedList();
+		
+		newLinkedList.add("Test1");
+		newLinkedList.add("Test2");
+		newLinkedList.add("Test3");
+		newLinkedList.add(null); // still get ignored
+		newLinkedList.add("Test4");
+		newLinkedList.add("Test5");
+
+		System.out.println("\n=== Test LinkedList ===\n");
+		System.out.println("\nPrint size and elements\n");
+
+		System.out.println(newLinkedList.size() + "\n");
+		System.out.println(newLinkedList.get(0).getReturnValue());
+		System.out.println(newLinkedList.get(1).getReturnValue());
+		System.out.println(newLinkedList.get(2).getReturnValue());
+		System.out.println(newLinkedList.get(3).getReturnValue());
+		System.out.println(newLinkedList.get(4).getReturnValue());
+
+		newLinkedList.remove(4);
+		newLinkedList.remove(3);
+		newLinkedList.remove(0);
+
+		System.out.println("\nPrint size and elements after removal\n");
+
+		System.out.println(newLinkedList.size() + "\n");
+
+		System.out.println(newLinkedList.get(0).getReturnValue());
+		System.out.println(newLinkedList.get(1).getReturnValue());
+		System.out.println(newLinkedList.get(2).getReturnValue());
+		System.out.println(newLinkedList.get(3).getReturnValue());
+		System.out.println(newLinkedList.get(4).getReturnValue());
+
+		System.out.println("\nTesting add at specific index point\n");
+
+
+		newLinkedList.add("Test6");
+		newLinkedList.add("Test7");
+		newLinkedList.add("Test8");
+		newLinkedList.add("Test9");
+
+		System.out.println(newLinkedList.size() + "\n");
+
+		newLinkedList.add(3, "Test10");
+
+		System.out.println(newLinkedList.get(3).getReturnValue());
+	}
+}

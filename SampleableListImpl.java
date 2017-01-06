33b56b60f1ba7ae06ac5e8847623a1258edd6547
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-01-06 12:33:17 +0000

    Adding Sampleable List exercise

---
+++ SampleableListImpl.java
@@ -0,0 +1,33 @@
+/**
+ * A sampleable list can be sampled. 
+ * 
+ * 6 - 	Write an implementation of interface SampleableList.
+ *
+ * @author Ginestra Ferraro
+ */
+
+public class SampleableListImpl extends ArrayList implements SampleableList {
+
+	/**
+	 * Returns a list containing the first, third, fifth...
+	 * items of this list, or an empty list if the list is empty. 
+	 * 
+	 * @return a list containing the first, third, fifth... items of this list
+	 */
+
+	public SampleableList sample() {
+
+		if(isEmpty()) {
+			return new SampleableListImpl();
+		} else {
+	        SampleableList oddList = new SampleableListImpl();
+
+	        for (int i = 0; i < size(); i++) {
+	            if ((i+1) % 2 != 0) {
+	                oddList.add(get(i).getReturnValue());
+	            }
+	        }
+	        return oddList;
+	    }
+	}
+}
---
+++ SampleableListImplTest.java
@@ -0,0 +1,34 @@
+/**
+ * A sampleable list can be sampled. 
+ * 
+ * 6 - 	Write an implementation of interface SampleableList.
+ *
+ * TEST
+ *
+ * @author Ginestra Ferraro
+ */
+
+public class SampleableListImplTest {
+	public static void main(String[] args) {
+		SampleableList oddListTest = new SampleableListImpl();
+
+		oddListTest.add("ZeroIndex");
+		oddListTest.add("UnoIndex");
+		oddListTest.add("DueIndex");
+		oddListTest.add("TreIndex");
+		oddListTest.add("QuattroIndex");
+		oddListTest.add("CinqueIndex");
+		oddListTest.add("SeiIndex");
+
+		System.out.println("\n=== Test oddList ===\n");
+
+		System.out.print("Index at 0: ");
+		System.out.println(oddListTest.sample().get(0).getReturnValue());
+		System.out.print("Index at 1: ");
+		System.out.println(oddListTest.sample().get(1).getReturnValue());
+		System.out.print("Index at 2: ");
+		System.out.println(oddListTest.sample().get(2).getReturnValue());
+		System.out.print("Index at 3: ");
+		System.out.println(oddListTest.sample().get(3).getReturnValue());
+	}
+}
